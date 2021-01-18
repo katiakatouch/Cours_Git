809d3a5174bd4fa77d59b2f5ab7755f6aecfeda0
unknown <kadikatia13@gmail.com>
2021-01-18 12:09:22 +0100

    Ajout de la deuxieme fonction

---
+++ Exercice1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir MOI
+cd MOI
+mkdir MesLettres
+mkdir MesDMs
+cd MesLettres
+touch Motiv.doc
+touch cv.doc
+cd /mnt/c/scripts/MOI
+mkdir MesTps
+cd MesTps
+mkdir TpsUNIX
+mkdir TpsC
+cd TpsC
+mkdir Executabes
+touch Tp1.c
+touch Tp2.c
